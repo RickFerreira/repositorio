f7045865f5564db3959b77dfe862ba1e0cba0ba5
Aluno <Aluno@DESKTOP-LDNVJ6K>
2018-08-24 08:47:17 -0300

    melhorado

---
+++ ProjetoJuan/src/main/java/dao/UsuarioDAO.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.util.List;
+
+import br.edu.ifpb.esperanca.daw2.filtros.UsuarioFiltro;
+import br.edu.ifpb.esperanca.daw2.ifoto.entities.Usuario;
+
+public class UsuarioDAO extends DAO<Usuario> {
+	
+	public UsuarioDAO() {
+		super(Usuario.class);
+	}
+
+	public List<Usuario> findBy(UsuarioFiltro filtro) {
+		return null;
+	}
+	
+}
---
+++ ProjetoJuan/src/main/java/services1/Service.java
@@ -0,0 +1,19 @@
+package services1;
+
+import java.util.List;
+
+import br.edu.ifpb.esperanca.daw2.ifoto.entities.Identificavel;
+
+public interface Service<E extends Identificavel> {
+
+	void save(E e);
+
+	void update(E e);
+
+	void remove(E e);
+
+	E getByID(long userId);
+
+	List<E> getAll();
+
+}
